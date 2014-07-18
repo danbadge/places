ef838671489eaf8cec19db4e248a0a6e2df96ff0
daniel <danbadge@gmail.com>
2014-07-18 11:48:08 +0100

    adding more info to marker infowindow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711125409) do
+ActiveRecord::Schema.define(version: 20140718103058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20140711125409) do
     t.string   "name"
     t.decimal  "longitude"
     t.decimal  "latitude"
-    t.string   "type"
+    t.string   "place_type"
     t.decimal  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
