20fbb9a61d73aa6379d764a42985cc67aed3c7f6
daniel <danbadge@gmail.com>
2014-07-18 12:02:15 +0100

    add place id column migration and previously missed rename of type

---
+++ db/migrate/20140718103058_rename_place_type_column_name.rb
@@ -0,0 +1,5 @@
+class RenamePlaceTypeColumnName < ActiveRecord::Migration
+  def change
+  	rename_column :places, :type, :place_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718103058) do
+ActiveRecord::Schema.define(version: 20140718105858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140718103058) do
     t.decimal  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "google_place_id"
   end
 
 end
