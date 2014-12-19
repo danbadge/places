706533f203f005f8a4a2569447368873a1ab5dd2
Daniel Watts <dwatts@eudwatts.local>
2014-12-19 16:59:10 +0000

    getting some int tests around retrieving a google place from places api using an Id

--- app/google_api/place_details.rb
+++ app/google_api/place_details.rb
@@ -1,34 +1,27 @@
 class PlaceDetails
 	def initialize
-		@api_key = "AIzaSyA6ww-54JxL_krZ0VyA5cCS8ALCgEowhss"
-		@base_url = "https://maps.googleapis.com/maps/api/"
-		@endpoint = "place/details/json"
+		@api_key = 'AIzaSyA6ww-54JxL_krZ0VyA5cCS8ALCgEowhss'
+		@base_url = 'https://maps.googleapis.com/maps/api/'
+		@endpoint = 'place/details/json'
 	end
 
 	def get_place(id)
 		request = @base_url + @endpoint + "?placeid=#{id}&key=#{@api_key}"
 		response = HTTParty.get(request)
-		puts response.code, response["status"]
-		unless response.code == 200 && response["status"] == "OK"
-			fail StandardError, "Could not find Google Place with Id: #{id}"
-		end
+		puts response.code, response['status']
+		fail StandardError, "Could not find Google Place with Id: #{id}" unless response.code == 200 && response["status"] == "OK"
 
-		place_details = response["result"]
+		place_details = response['result']
 
 		place = Place.new()
-		place.name = place_details["name"]
-		location = place_details["geometry"]["location"]
-		place.latitude = location["lat"]
-		place.longitude = location["lng"]
-		place.rating = place_details["rating"]
-		place.place_type = ''
+		place.name = place_details['name']
+		place.google_place_id = place_details['place_id']
+		location = place_details['geometry']['location']
+		place.latitude = location['lat']
+		place.longitude = location['lng']
+		place.rating = place_details['rating']
+		place.place_type = place_details['types'].join(',')
 
-		place_details["types"].each do |type|
-			if place.place_type.to_s.length > 0
-	  			place.place_type = place.place_type + ", "
-			end
-			place.place_type = place.place_type + "#{type}"
-		end
 		return place
 	end
 
---
+++ spec/integration/google_api/place_details_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'retrieving a place from google api' do
+
+  it 'should return the correct values' do
+    placesApi = PlaceDetails.new()
+    placeId = 'ChIJe29QNB0QdkgR5Kkx_0-jdo4'
+    place = placesApi.get_place(placeId)
+
+    expect(place).to be_a_kind_of(Place)
+    expect(place.google_place_id).to eq(placeId)
+    expect(place.name).to eq('The Ledbury')
+    expect(place.longitude).to eq(-0.200106)
+    expect(place.latitude).to eq(51.516618)
+    expect(place.place_type).to eq("bar,restaurant,food,establishment")
+    expect(place.rating).to eq(4.7)
+  end
+end
