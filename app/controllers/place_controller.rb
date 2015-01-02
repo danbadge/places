25f3dbdbb61896d14ae5e525eb29d489436f608c
danielw <dwatts@thoughtworks.com>
2015-01-02 18:08:46 +0000

    use root url

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -4,7 +4,7 @@ class PlaceController < ApplicationController
   def post
   	place = request.body.to_json
     respond_to do |format|
-      format.json { render json: place, status: 201, location: 'http://localhost:3000/place/' + place['google_place_id'] }
+      format.json { render json: place, status: 201, location: "#{root_url}place/#{place['google_place_id']}" }
     end
   end
 end
