b2569ea1b4bfce6cfe7d679da38a58fd1f4459c7
danielw <dwatts@thoughtworks.com>
2015-01-06 13:48:42 +0000

    removing redundant codes

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,24 +1,11 @@
 class MapController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => [:create]
-  before_filter :setup_google_api
-
-  def setup_google_api
-    @google_api = PlaceDetails.new()
-  end
-
   def index
   	@places = Array.new
 
   	Place.all.each do |place|
-  		if place.name != 'test place'
-        @places.push(place)
-  		end
+      @places.push(place)
   	end
 
   	return @places
-  end
-
-  def get_place(id)
-    return @google_api.get_place(id)
   end
 end
